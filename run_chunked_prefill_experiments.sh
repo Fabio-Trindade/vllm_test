7f0f6076fd618bb233124afedf5bf9b27d1fbbbd
Fabio-Trindade <fabiotrindadefer@gmail.com>
2025-03-11 18:37:29 -0300

    updating sh

--- run_chunked_prefill_experiments.sh
+++ run_chunked_prefill_experiments.sh
@@ -1,19 +1,20 @@
-model=facebook/opt-125M
+model=llama/Llama-2-7B-hf-F16.gguf
 time=1
 sleep_time=0.1
 
 # without enable chunked prefill
 vllm serve ${model} --port 8000 &
 PID1=$!
+echo $PID1
 sleep 120
 python3 vllm_client.py  --model ${model} --time ${time} --sleep-time ${sleep_time}
 
-# Mata o servidor após o uso
 kill $PID1
 
 # with enable chunked prefill
 vllm serve $model --port 8000 --enable-chunked-prefill &
 PID2=$!
+echo $PID2
 sleep 120
 python3 vllm_client.py  --model ${model} --time ${time} --sleep-time ${sleep_time} --using_chunked_prefill
 
