178840d15def8980b9bf786a4215bdbb8890d54d
Fabio-Trindade <fabiotrindadefer@gmail.com>
2025-04-01 06:36:13 -0300

    run

--- run_apc_experiment.sh
+++ run_apc_experiment.sh
@@ -4,7 +4,7 @@ tb_base_dir=./results/tensorboard_logs/
 
 model=./llama/Llama-2-7B-hf-F16.gguf
 
-python3 dynamic_cached_vs_uncached_experiment.py  --model $model  --tokenizer_path ${BASE_DIR}/llama --tensorboard_log_pathname ${tb_base_dir}cached/
+#python3 dynamic_cached_vs_uncached_experiment.py  --model $model  --tokenizer_path ${BASE_DIR}/llama 
 
-python3 constant_cached_vs_uncached_experiment.py --model $model  --tokenizer_path ${BASE_DIR}/llama --tensorboard_log_pathname ${tb_base_dir}cached/
-python3 constant_cached_vs_uncached_experiment.py --model $model --tokenizer_path ${BASE_DIR}/llama --tensorboard_log_pathname ${tb_base_dir}uncached/ --reverse
+python3 constant_cached_vs_uncached_experiment.py --model $model  --tokenizer_path ${BASE_DIR}/llama 
+python3 constant_cached_vs_uncached_experiment.py --model $model --tokenizer_path ${BASE_DIR}/llama --reverse
